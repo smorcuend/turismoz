2df991ae5ebb2eddc98727a9ffdb547362d5462e
smorcuend <smorcuend@smorcuend-laptop.(none)>
2013-01-26 18:02:04 +0100

    Initial commit

---
+++ js/application.js
@@ -0,0 +1,42 @@
+
+$('#ir').click(function(event){
+
+  var stringBuffer = $('#inputdata input').attr('value');
+  console.log(stringBuffer);
+  searchPlace(stringBuffer);
+
+});
+
+$('#inputdata irgps').click(function(){
+	console.log('button pulsado');
+	getLocation();
+	
+});
+
+$("#city").jeoCityAutoComplete();
+
+function searchPlace(place){
+
+	if(typeof(place) == "string"){
+
+		if(place == "Madrid"){
+			
+		}
+
+	}
+
+}
+
+function getLocation(){
+	if (navigator.geolocation){
+    	navigator.geolocation.getCurrentPosition(showPosition);
+	}else{
+		console.log("Geolocation is not supported by this browser.");
+	}
+}
+
+function showPosition(position){
+	//x.innerHTML="Latitude: " + position.coords.latitude +"<br>Longitude: " + position.coords.longitude;
+	$('#grid').text("Latitude: " + position.coords.latitude +" , Longitude: " + position.coords.longitude);
+	console.log("Latitude: " + position.coords.latitude +" , Longitude: " + position.coords.longitude);
+}
